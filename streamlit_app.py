3db0d84e6b476c81d57a712471c047ab6146911c
ProyectoDatos <106175102+pablo2343567@users.noreply.github.com>
2022-05-24 18:38:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 import operator
-import matplotlib.pyplot as plt
+import matplotlib as plt
 import numpy as np
 import pygal
 from pygal.style import Style
