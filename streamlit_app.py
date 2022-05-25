ff95b8a532fa0e5485f72b99bc071c50c56c91a8
ProyectoDatos <106175102+pablo2343567@users.noreply.github.com>
2022-05-25 18:58:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,11 +13,11 @@ import xlrd
 from pygal.style import Style
 
 
-df = pd.read_csv('https://raw.githubusercontent.com/pablo2343567/streamlit-example/master/d99.csv',na_values='NA', keep_default_na=False)
+df = pd.read_csv('https://raw.githubusercontent.com/pablo2343567/streamlit-example/master/d99.csv',delimiter=';')
 
-df99 = pd.read_csv('https://raw.githubusercontent.com/pablo2343567/streamlit-example/master/d.csv',na_values='NA', keep_default_na=False)
+df99 = pd.read_csv('https://raw.githubusercontent.com/pablo2343567/streamlit-example/master/d.csv',delimiter=';')
 
-dft = pd.read_csv('https://raw.githubusercontent.com/pablo2343567/streamlit-example/master/t.csv',na_values='NA', keep_default_na=False)
+dft = pd.read_csv('https://raw.githubusercontent.com/pablo2343567/streamlit-example/master/t.csv',delimiter=';')
 
 
 dft
