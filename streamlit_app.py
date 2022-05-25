64a2b44e879fb7fbe082db28b1d1c91605b66b8a
ProyectoDatos <106175102+pablo2343567@users.noreply.github.com>
2022-05-25 21:22:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-from collections import namedtuple
-import altair as alt
-import math
+
 
 import operator
 import matplotlib as plt
@@ -11,8 +9,7 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 import pygal
-import xlrd
-from pygal.style import Style
+
 
 
 df = pd.read_csv('https://raw.githubusercontent.com/pablo2343567/streamlit-example/master/d99.csv',delimiter=';')
