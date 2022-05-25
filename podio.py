9ac1da01cd2423757f57367084851c2eb30e87aa
ProyectoDatos <106175102+pablo2343567@users.noreply.github.com>
2022-05-25 21:19:41 +0200

    Update podio.py

--- podio.py
+++ podio.py
@@ -296,9 +296,6 @@ def graficaPodioDeporte (deport,listadeportes,listapaises):
 opcion = st.selectbox('Selecciona una modalidad',listadeportes)
 
 
-
-
-
-
-
 graficaPodioDeporte(opcion,listadeportes,listapaises)
+
+st.text ('El valor de 3 corresponde al primer puesto, 2 para el segundo y 3 para el tercer puesto')
