e5d05c339d3dfaddd0e203100ab787c9b9237279
ProyectoDatos <106175102+pablo2343567@users.noreply.github.com>
2022-05-25 18:13:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,12 @@ import streamlit as st
 import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/pablo2343567/streamlit-example/master/prueba.csv')
+
+
 df.head()
-a = df.tolist()
+
+
+
 
 
 
