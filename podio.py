275f51c14d95831cb9b24d2f333587f9d2646260
ProyectoDatos <106175102+pablo2343567@users.noreply.github.com>
2022-05-25 21:14:32 +0200

    Update podio.py

--- podio.py
+++ podio.py
@@ -1,1 +1,306 @@
 
+import streamlit as st
+import pandas as pd
+
+from collections import namedtuple
+import altair as alt
+import math
+
+import operator
+import matplotlib as plt
+import matplotlib.pyplot as plt
+
+import numpy as np
+import pygal
+import xlrd
+from pygal.style import Style
+
+
+df = pd.read_csv('https://raw.githubusercontent.com/pablo2343567/streamlit-example/master/d99.csv',delimiter=';')
+
+df99 = pd.read_csv('https://raw.githubusercontent.com/pablo2343567/streamlit-example/master/d.csv',delimiter=';')
+
+dft = pd.read_csv('https://raw.githubusercontent.com/pablo2343567/streamlit-example/master/t.csv',delimiter=';')
+
+
+
+listadeportes = list (dft['Paises'])
+listapaises = list (df['Paises'])
+ 
+listaaños = ['R-' , '17' , '18' , '19' , 'T-']
+
+
+
+
+def ListaSoloGanadores():
+    """
+    Devuelve una lista de diccionarios con clave el deporte y valor el ganador
+    """
+    res = {}
+    for deporte in listadeportes:
+
+        for i in range (len(listapaises)): 
+            pos = int ( df99[deporte][i] )
+            if pos == 1:
+                res[deporte] = listapaises[i]
+                
+    return res
+
+
+listaGanadores = ListaSoloGanadores()
+
+
+
+
+
+def filtro (cadena):
+    """
+    Dada una cadena de texto, selecciona los deportes que la contienen
+    """
+    l = []
+    for deporte in listadeportes:
+        if cadena in deporte:
+            l.append(deporte)
+    
+    return l
+
+
+def nGanadores(deportes,paises,n=20):
+    """
+    Devuelve un número n de ganadores por cada deporte. Devuelve un diccionario
+    con clave el deporte y con valor un diccionario con clave el país y valor
+    la posición de dicho país. 
+    """
+    res = {}
+    for deporte in deportes:
+        dicpos = {}
+        
+        for i in range (len(paises)): 
+            pos = int ( df99[deporte][i] )
+            if pos != 99 and pos != 0 and pos <= n:
+                dicpos[paises[i]] = pos
+
+        valores_ord = dict(sorted(dicpos.items(), key=operator.itemgetter(1)))
+        
+        res[deporte] = valores_ord
+                
+    return res
+            
+
+
+
+def nGanadoresInversa(deportes,paises,n=12):
+        """
+        Como el diccionario de nGanadores pero da más peso a los primeros 
+        """
+        a = list (range(1,n+1))
+        
+        res = {}
+        for deporte in deportes:
+            dicpos = {}
+            
+            for i in range (len(paises)): 
+                pos = int ( df99[deporte][i] )
+                if pos != 99 and pos != 0 and pos <= n:
+                    dicpos[paises[i]] = a[-pos]
+    
+            valores_ord = dict(sorted(dicpos.items(), key=operator.itemgetter(1)))
+            
+            res[deporte] = valores_ord
+                    
+        return res
+
+
+
+
+
+
+def PaisDeporte (deportes,paises,n=20):
+    """
+    Diccionario con clave los países y valores un diccionario con el deporte
+    y la posición en la que ha quedado. La n indica hasta qué posición se
+    aceptan los deportes (con n=2 se seleccionan la primera y la segunda pos)
+    """
+    res = {}
+    for i in range (len(paises)): 
+        pais = paises[i]
+        dicpos = {}
+        
+        for deporte in deportes:
+            
+            pos = int ( df99[deporte][i] )
+            if pos != 99 and pos != 0 and pos<=n:
+                dicpos[deporte] = pos
+                
+        valores_ord = dict(sorted(dicpos.items(), key=operator.itemgetter(1)))
+        
+        res[pais] = valores_ord
+
+    return res
+
+
+
+
+def cantidadMedallas(dic):
+    """
+    Dado un diccionario de PaisDeporte, devuelve la cantidad de medallas
+    de cada país
+    """
+    res = {}
+    for pais in dic:
+        res[pais] = len(dic[pais])
+        
+    return res
+
+
+
+
+    
+def dicEvolucionNuevo(deporte, dic):
+        listaDeporte = filtro(deporte)
+        d={}
+        
+        def resetLista():
+            long = len(listaDeporte)
+            lista = []
+            for i in range (long):
+                lista.append(0)  
+            return lista
+        
+        
+        lista = resetLista()
+        
+        i = 0
+        for comp in listaDeporte:
+
+            
+            posiciones=dic[comp]
+            for pais, pos in posiciones.items():
+                if pais not in d:
+                    
+                    lista = resetLista()
+                    
+                    lista[i] = pos
+                    d[pais] = lista.copy()
+                else:
+                    acum = d[pais]
+                    acum[i] = pos
+                    d[pais] = acum
+            i += 1
+        return d
+    
+    
+    
+    
+d1 = nGanadores(listadeportes,listapaises)
+d2 = PaisDeporte(listadeportes,listapaises,3)
+
+d4 = nGanadoresInversa (listadeportes,listapaises)
+
+d5 = cantidadMedallas(d2)
+
+
+
+
+
+
+
+
+
+
+#----------------------------------------------------------
+
+
+        # FUNCIONES PARA LAS GRÁFICAS
+
+
+#--------------------------------------------------------
+
+
+
+
+def graficaPodioDeporte (deport,listadeportes,listapaises):
+    """
+    Dado un deporte, dibuja un podio to wapo
+    """
+         
+    def dicPuesto(deportes,paises):
+        """
+        Como el diccionario de nGanadores pero da más peso a los primeros 
+        """
+        a = list (range(1,4))
+        
+        res = {}
+        for deporte in deportes:
+            dicpos = {}
+            
+            for i in range (len(paises)): 
+                pos = int ( df99[deporte][i] )
+                if pos != 99 and pos != 0 and pos <= 3:
+                    dicpos[paises[i]] = a[-pos]
+    
+            valores_ord = dict(sorted(dicpos.items(), key=operator.itemgetter(1)))
+            
+            res[deporte] = valores_ord
+                    
+        return res
+        
+    
+    
+    dic = dicPuesto(listadeportes,listapaises)
+    
+    
+    a = filtro(deport)
+    x = len(a)
+    i = 0
+    for deporte in dic:
+        if deporte in a:
+            i += 1
+            plt.figure(figsize=(10,8))
+            plt.title(deporte)
+            result = dic[deporte]
+            result = pd.Series(result)
+            
+            coloritos = [0,0,0]
+            valores = [0,0,0]
+            indices = ['','','']
+
+            for i in range (len(result.values)):
+                
+                x = result.values[i]
+                z = x   
+                if x == max(result.values):
+                    coloritos[1] = 'gold'
+                    valores[1] = z
+                    indices[1] = result.index[i]
+                elif x == min(result.values):
+                    coloritos[2] = 'sienna'
+                    valores[2] = z
+                    indices[2] = result.index[i]
+                else:
+                    coloritos[0] = 'grey'
+                    valores[0] = z
+                    indices[0] = result.index[i]
+            plt.bar(indices, valores , color= coloritos)
+            plt.show()
+
+
+
+
+    
+   
+
+
+agree = st.checkbox('¿Quieres seleccionar los deportes por código? Ej: escribir M para todas las modalidades de Masculino', value=False)                    
+                    
+listaopciones = ['A4','Baloncesto','Balonmano','Ciclo','Doma','Espada','Florete','Futbol','Gim','Hockey','K4','N','Rugby','Sable','Salto','Volei','Wax','Waterpolo']
+
+if not agree:
+    opcion = st.selectbox('Selecciona uno de los siguientes deportes:',options=listaopciones,index = 2)
+else:
+    opcion = st.text_input('Escribe un deporte codificado:')
+
+
+
+
+n = st.select_slider('Selecciona la cantidad de países a mostrar', options=range(1,9),value = 4)
