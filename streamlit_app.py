71e2c416fc465267360114b3c55391f0bac82a7d
ProyectoDatos <106175102+pablo2343567@users.noreply.github.com>
2022-05-25 18:29:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv('https://raw.githubusercontent.com/pablo2343567/streamlit-example/master/prueba2.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/pablo2343567/streamlit-example/master/d.csv')
 
 
 df
