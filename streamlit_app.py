6fff1212a2bf81ff07d7c524f3ac8e3655ef82a1
ProyectoDatos <106175102+pablo2343567@users.noreply.github.com>
2022-05-25 17:56:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,5 @@ import pygal
 import xlrd
 from pygal.style import Style
 
-url = 'https://github.com/pablo2343567/streamlit-example/blob/master/datos.csv?raw=true'   
+url = 'https://raw.github.com/pablo2343567/streamlit-example/blob/master/datos.csv?raw=true'   
 df = pd.read_csv(url,index_col=0)
